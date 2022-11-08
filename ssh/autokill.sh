8c98b23e91bbe3d05b645f31c973dc8afebf1e88
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2022-11-08 15:48:25 +0700

    Update autokill.sh

--- ssh/autokill.sh
+++ ssh/autokill.sh
@@ -24,6 +24,8 @@ echo -e "[1]  AutoKill After 5 Minutes"
 echo -e "[2]  AutoKill After 10 Minutes"
 echo -e "[3]  AutoKill After 15 Minutes"
 echo -e "[4]  Turn Off AutoKill/MultiLogin"
+echo -e "[5]  AutoKill After 2 Minutes(new)"
+echo -e "[x]  BACK TO MENU"
 echo ""
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
@@ -97,10 +99,28 @@ case $AutoKill in
                 service cron restart >/dev/null 2>&1
                 service cron reload >/dev/null 2>&1
                 ;;
+                1)
+                echo -e ""
+                sleep 5
+                clear
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >/etc/cron.d/tendang
+                echo "*/2 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 2 Minutes"      
+                echo -e ""
+                echo -e "======================================"                                                                                                                                 
+                service cron restart >/dev/null 2>&1
+                service cron reload >/dev/null 2>&1                                                                  
+                ;;
                 x)
                 clear
                 autokill-menu
                 ;;
+                x) clear ; menu ; exit ;;
         esac
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
