03e7acb46f2baa1d3cdfade5265342416e7e0ec7
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2022-11-08 20:06:54 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,20 +4,20 @@
 cd
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/openssh-socket.py
+#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/ws-stunnel
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/openssh-socket.py
 
 #izin permision
-chmod +x /usr/local/bin/ws-openssh
+#chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
-chmod +x /usr/local/bin/ws-ovpn
+#chmod +x /usr/local/bin/ws-ovpn
 
 
 #System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
+#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/H-Pri3l/xray-publikmain/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
@@ -26,15 +26,15 @@ wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.co
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/H-Pri3l/xray-publik/main/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 #System Websocket-OpenVPN Python
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+#wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 
 #restart service
 
 systemctl daemon-reload
-Enable & Start & Restart ws-openssh service
-systemctl enable ws-openssh.service
-systemctl start ws-openssh.service
-systemctl restart ws-openssh.service
+#Enable & Start & Restart ws-openssh service
+#systemctl enable ws-openssh.service
+#systemctl start ws-openssh.service
+#systemctl restart ws-openssh.service
 
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
@@ -47,6 +47,6 @@ systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
 
 #Enable & Start & Restart ws-ovpn service
-systemctl enable ws-ovpn.service
-systemctl start ws-ovpn.service
-systemctl restart ws-ovpn.service
+#systemctl enable ws-ovpn.service
+#systemctl start ws-ovpn.service
+#systemctl restart ws-ovpn.service
